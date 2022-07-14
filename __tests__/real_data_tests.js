f2dfaab8c2054f5d3b053f7cc1c007f349d3e976
Cat Smith <910384+catsmith@users.noreply.github.com>
2022-07-14 18:11:30 +0100

    full xml tests working

--- __tests__/real_data_tests.js
+++ __tests__/real_data_tests.js
@@ -21,9 +21,23 @@ let xmlData;
 beforeAll(async () => {
     xmlData = new Map();
     const filenames = await fs.promises.readdir(dir);
+    const expectedHeader = '<TEI><teiHeader></teiHeader>';
     for (let file of filenames) {
-        const data = await fs.promises.readFile(dir + file);
-        xmlData.set(file, data.toString().replace(/\n/g, '').replace(/\s+/g, ' ').replace(/> </g, '><').replace(/" >/g, '">'));   
+        let data = await fs.promises.readFile(dir + file);
+        // remove unnecessary spaces from the data files
+        data = data.toString().replace(/\n/g, '').replace(/\s+/g, ' ').replace(/> </g, '><').replace(/" >/g, '">');
+        // remove the sigla from breaks
+        data = data.replace(/-(P|L)?\d+S?"/g, '-"');
+        // remove the sigla from the notes (why is it even here!)
+        data = data.replace(/-(P|L)?\d+S?-/g, '--');
+        // uncollapse empty readings
+        data = data.replace(/<rdg ([^>]+)\/>/g, '<rdg $1></rdg>')
+        // remove the header
+        header = data.substring(0, data.indexOf('</teiHeader>') + 12);
+        data = String(data.replace(header, expectedHeader));
+        // remove the language declaration
+        data = data.replace(/<text xml:lang="\S+">/, '<text>');      
+        xmlData.set(file, data);   
     } 
 });
 
@@ -48,6 +62,10 @@ test('xml files', () => {
         expectedOutput = value;
         testDOM.window.eval(`setTEI('${testInput}');`);
         tei = testDOM.window.eval(`getTEI();`);
+        // we need to run this on the result because the numbers get taken out of the note xml:ids in the remove sigla regex above
+        tei = tei.replace(/-(P|L)?\d+S?"/g, '-"');
+        header = tei.substring(0, tei.indexOf('</teiHeader>') + 12); 
+        tei = String(tei.replace(header, '<TEI><teiHeader></teiHeader>'));
         expect(tei).toBe(expectedOutput);
     }
 });
